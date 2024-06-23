ee9b704c8a04ea0c6e17f5909e7197f11d2772c8
rajeshp2408 <rajeshp2408@gmail.com>
2024-06-23 21:05:02 +0800

    project commit

---
+++ 1_Python_API_Using_Flask/main.py
@@ -0,0 +1,38 @@
+import flask
+from flask import jsonify, request
+
+app= flask.Flask(__name__)
+app.config["DEBUG"] = True #Can defind debug 1
+
+games=[{'console': 'PS5',
+       'name': 'Call of Duty'},
+      {'console': 'XBOX',
+       'name': 'GTA 5'},
+      {'console': 'XBOX',
+       'name': 'Minecraft'},
+      {'console': 'XBOX',
+       'name': 'Mad Max'},
+      {'console': 'Nintendo Switch',
+       'name': 'Super Mario Bros.™ Wonder'},
+      {'console': 'Nintendo Switch',
+       'name': "Luigi's Mansion™ 2 HD"},
+      {'console': 'Nintendo Switch',
+       'name': "Mario Kart™ 8 Deluxe"}]
+
+# @app.route("/api", methods =['GET'])
+# def  run_app():
+#     return jsonify(games)
+
+@app.route("/api", methods =['GET'])
+def  getGames():
+    console=request.args['console']
+    result =[]
+    for game in games:
+        if game['console']== console:
+            result.append(game)
+
+    return jsonify(result)
+
+if __name__=="__main__":
+    # app.run(host="127.0.0.1", port= 5571, debug= True) #Define Debug 2
+    app.run(host="127.0.0.1", port= 5571)
