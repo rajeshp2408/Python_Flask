dd2ba8844a4afcb36487e289c31661ae469dc28e
rajeshp2408 <rajeshp2408@gmail.com>
2024-07-08 23:09:35 +0800

    template inheritance

--- 1_Market_App_Flask/main.py
+++ 1_Market_App_Flask/main.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/market")
 def market_page():
